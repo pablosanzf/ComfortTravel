ee4284c973c631c4952a4927281925c997835bf2
pablosanzf <pablosanzf@gmail.com>
2017-11-16 09:53:14 +0100

    Share options added

--- app/src/main/java/pablosanzf/comforttravel/AsientoActivity.java
+++ app/src/main/java/pablosanzf/comforttravel/AsientoActivity.java
@@ -3,17 +3,24 @@ package pablosanzf.comforttravel;
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -30,37 +37,39 @@ public class AsientoActivity extends Activity implements
     private static final  Asiento LECTURA = new Asiento(new String("LECTURA"), new String("Lectura"), 20,20 ,20,20,20);
     private static final  Asiento SEGURIDAD = new Asiento(new String("SEGURIDAD"), new String("Seguridad"), 30,30 ,30,30,30);
 
+    private ArrayList<String>  listaDePerfiles = new ArrayList<String>();
+
+    ArrayAdapter<String> listnav;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Aquí debería hacerse la recogida del intent desde la fase anterior de sincornización con el asiento
+        this.asiento = new Asiento("", "Manual", 0,0,0,0,0);
+
+        perfilesDeAsiento.add(asiento);
+
         perfilesDeAsiento.add(SEGURIDAD);
         perfilesDeAsiento.add(NOCHE);
         perfilesDeAsiento.add(LECTURA);
 
-
-        //Aquí debería hacerse la recogida del intent desde la fase anterior de sincornización con el asiento
-        this.asiento = new Asiento("", "Manual", 0,0,0,0,0);
-
-        System.err.println("he llegado hasta aqui");
+        listaDePerfiles = llenarArrayConPerfiles(perfilesDeAsiento);
 
         setContentView(R.layout.activity_asiento);
-
-
-        System.err.println("he llegado hasta aqui- 2");
 
         ActionBar actionBar = getActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActionBarThemedContextCompat(),
-                R.array.perfiles, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        actionBar.setListNavigationCallbacks(adapter,this);
+
+        listnav = new ArrayAdapter<String>(getActionBarThemedContextCompat(), android.R.layout.simple_spinner_item , listaDePerfiles);
+        listnav.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actionBar.setListNavigationCallbacks(listnav,this);
 
         onNavigationItemSelected(0,0);
-        asiento = buscarEnArray(getResources().getStringArray(R.array.perfiles)[0]);
+        asiento = buscarEnArray(listaDePerfiles.get(0));
 
 
 
@@ -105,6 +114,19 @@ public class AsientoActivity extends Activity implements
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.asiento, menu);
+
+        MenuItem mnuShare = menu.findItem(R.id.mnu_compartir);
+        ShareActionProvider shareProv = (ShareActionProvider)
+                mnuShare.getActionProvider();
+        shareProv.setShareHistoryFileName(ShareActionProvider.DEFAULT_SHARE_HISTORY_FILE_NAME);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        String mensajeParaEnviar = new String("Prueba a viajar de esta manera \r\n Reposapies: " + asiento.getRotacionReposapies() + " \r\n Respaldo: " + asiento.getRotacionAsiento()
+                                                        + "  \r\n Reposacabezas: " + asiento.getRotacionCabeza() + "\r\n Luminosidad: " + asiento.getLuminosidad() + " \r\n Temperatura: " + asiento.getTemperatura()
+                                                        + "\r\n ¡Cómo mola viajar con ComfortTravel!");
+        intent.putExtra(Intent.EXTRA_TEXT, mensajeParaEnviar);
+        shareProv.setShareIntent(intent);
+
         return true;
     }
 
@@ -114,10 +136,85 @@ public class AsientoActivity extends Activity implements
     public boolean onOptionsItemSelected(MenuItem item) { //Otra forma de hacer lo mismo que se hace hacia arriba
         if(item.getItemId()==R.id.mnu_guardar){
 
+            final AlertDialog.Builder dialogoGuardar = new AlertDialog.Builder(AsientoActivity.this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.dialog_guardar_perfil, null);
+            dialogoGuardar.setView(dialogView);
+
+            dialogoGuardar.setTitle("Nombre del nuevo perfil");
+            final EditText nuevoModo = (EditText) dialogView.findViewById(R.id.editText_nombre_perfil_nuevo);
+            dialogoGuardar.setPositiveButton("Hecho", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (nuevoModo.getText().toString().equals("")) {
+                        Toast.makeText(getApplicationContext(), "Add a value", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (listaDePerfiles.indexOf(nuevoModo.getText().toString()) != -1) {
+                            Toast.makeText(getApplicationContext(), "Ya existe un perfil con ese nombre", Toast.LENGTH_SHORT).show();
+                        } else {
+                            System.out.println("aiento a añadir: " + asiento);
+                            listaDePerfiles.add(nuevoModo.getText().toString());
+                            System.out.println(perfilesDeAsiento);
+                            asiento.setNombreModo(nuevoModo.getText().toString());
+                            perfilesDeAsiento.add(asiento);
+                            System.out.println(perfilesDeAsiento);
+                            llenarArrayConPerfiles(perfilesDeAsiento);
+
+                            System.out.println("tamaño del array de asientos " + perfilesDeAsiento.size() + " y el array de strings " + listaDePerfiles);
+                            System.out.println("y el asiento que hay ahora es  " + asiento);
+
+                            listnav.notifyDataSetChanged();
+
+                            Toast.makeText(getApplicationContext(), "Perfil añadido", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+            dialogoGuardar.setNegativeButton("Cancelar",new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick (DialogInterface dialogInterface,int i){
+                        }
+                    }
+            );
+            dialogoGuardar.show();
             return true;
         }
         if(item.getItemId()==R.id.mnu_borrar){
 
+            AlertDialog.Builder builder = new AlertDialog.Builder(AsientoActivity.this);
+            builder.setMessage("¿Desea borrar el perfil " + asiento.getNombreModo().toLowerCase() + " ?");
+            builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (asiento.getNombreModo().equals("Manual")){
+                        Toast.makeText(getApplicationContext(), "No se puede borrar el perfil manual", Toast.LENGTH_SHORT).show();
+                    }else{
+                        System.out.println("aiento a borrar: " + asiento);
+                        listaDePerfiles.remove(asiento.getNombreModo());
+                        System.out.println(perfilesDeAsiento);
+                        perfilesDeAsiento.remove(asiento);
+                        System.out.println(perfilesDeAsiento);
+                        llenarArrayConPerfiles(perfilesDeAsiento);
+                        asiento = perfilesDeAsiento.get(0);
+                        System.out.println("tamaño del array de asientos " + perfilesDeAsiento.size() + " y el array de strings " + listaDePerfiles);
+                        System.out.println("y el asiento que hay ahora es  " + asiento);
+                        onNavigationItemSelected(0,0);
+                        listnav.notifyDataSetChanged();
+                        onNavigationItemSelected(0,0);
+                        listnav.notifyDataSetChanged();
+                        Toast.makeText(getApplicationContext(), "boton de sí", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    Toast.makeText(getApplicationContext(), "boton de no", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
             return true;
         }
         return false;
@@ -128,11 +225,9 @@ public class AsientoActivity extends Activity implements
     @Override
     public boolean onNavigationItemSelected(int itemPosition, long itemId) {
 
-
-        Asiento elegido = buscarEnArray(getResources().getStringArray(R.array.perfiles)[itemPosition]);
-        modificarValoresAsiento (elegido);
-
-
+        //Asiento elegido = buscarEnArray(getResources().getStringArray(R.array.perfiles)[itemPosition]);
+        asiento = buscarEnArray(listaDePerfiles.get(itemPosition));
+        modificarValoresAsiento (asiento);
 
         invalidateOptionsMenu(); //esta invocación sirve para destuir el menu y dejarlo guardado, para asi por si se comparte tener en el intent de compartir
         //el nuevo valor de grados y de ciudad
@@ -144,8 +239,18 @@ public class AsientoActivity extends Activity implements
      * @param elegido el asiento con los valores del perfil elegido
      */
     private void modificarValoresAsiento(Asiento elegido) {
-        Toast.makeText(this, "ambiando al perfil " + elegido.getNombreModo(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Cambiando al perfil " + elegido.getNombreModo() + " temperatura= " + elegido.getTemperatura(), Toast.LENGTH_SHORT).show();
     }
+
+    private ArrayList<String> llenarArrayConPerfiles (ArrayList<Asiento> perfilesDeAsiento){
+        ArrayList<String> respuesta = new ArrayList<String>();
+        for (int i=0; i<perfilesDeAsiento.size(); i++){
+            respuesta.add(perfilesDeAsiento.get(i).getNombreModo());
+        }
+
+        return respuesta;
+    }
+
 
 
     private Asiento buscarEnArray (String perfil){
